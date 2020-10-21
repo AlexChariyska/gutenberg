ae27a07fee2495de238f00957cf28ca176ada9c9
Alexandra Chariyska <alex.chariyska@gmail.com>
2020-10-21 16:27:20 +0300

    Patch Current UnitControl

--- packages/components/src/number-control/index.js
+++ packages/components/src/number-control/index.js
@@ -156,7 +156,10 @@ export function NumberControl(
 			type === inputControlActionTypes.PRESS_ENTER ||
 			type === inputControlActionTypes.COMMIT
 		) {
-			state.value = roundClamp( currentValue, min, max );
+			state.value =
+				currentValue === 'auto'
+					? currentValue
+					: roundClamp( currentValue, min, max );
 		}
 
 		return state;
